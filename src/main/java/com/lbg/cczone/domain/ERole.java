402932fd05f86dc8deec5b8255c4484169d7f85e
AndrewClark399 <andy396clark@hotmail.co.uk>
2024-03-27 16:15:19 +0000

    new branch for testing

--- src/main/java/com/lbg/cczone/domain/ERole.java
+++
@@ -1,5 +0,0 @@
-package com.lbg.cczone.domain;
-
-public enum ERole {
-	ROLE_USER, ROLE_MODERATOR, ROLE_ADMIN
-}
--- src/test/java/com/lbg/cczone/selenium/ItemTest.java
+++ src/test/java/com/lbg/cczone/selenium/ItemTest.java
@@ -1,7 +1,6 @@
 package com.lbg.cczone.selenium;
 
 import java.time.Duration;
-import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,8 +12,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.jdbc.Sql;
@@ -36,18 +33,21 @@ public class ItemTest {
 	}
 
 	@Test
-	@Order(2)
+	@Order(1)
 	void createItemPage() {
 		this.driver.get("http://localhost:3000/item");
 
-		WebElement newItemName = this.driver.findElement(By.cssSelector("root > div > form > input:nth-child(3)"));
+		WebElement newItemName = this.driver.findElement(By.cssSelector("#root > div > form > input:nth-child(3)"));
 		newItemName.sendKeys("Biscuit");
 
-		WebElement newItemPrice = this.driver.findElement(By.cssSelector("root > div > form > input:nth-child(6)"));
+		WebElement newItemPrice = this.driver.findElement(By.cssSelector("#root > div > form > input:nth-child(6)"));
 		newItemPrice.sendKeys("4.50");
 
 		WebElement newItemQuantity = this.driver.findElement(By.cssSelector("#root > div > form > input:nth-child(9)"));
 		newItemQuantity.sendKeys("5");
+
+		WebElement newImage = this.driver.findElement(By.id("propertyUploadImages"));
+		newImage.sendKeys("image");
 
 		WebElement submit = this.driver.findElement(By.id("itemSubmit"));
 //		submit.click();
@@ -57,37 +57,37 @@ public class ItemTest {
 	}
 
 	@Test
-	@Order(1)
+	@Order(2)
 	void getItems() {
 		this.driver.get("http://localhost:3000/item");
 
-		WebElement createdItemName = this.driver.findElement(
-				By.cssSelector("body > div > div:nth-child(3) > div > div > div:nth-child(1) > div > p:nth-child(2)"));
-		Assertions.assertEquals("ITEM : Cake", createdItemName.getText());
+		WebElement createdItemName = this.driver
+				.findElement(By.cssSelector("#root > div > div > div > div > div > div > h5"));
+		Assertions.assertEquals("Cake", createdItemName.getText());
 
-		WebElement createdPrice = this.driver.findElement(
-				By.cssSelector("body > div > div:nth-child(3) > div > div > div:nth-child(1) > div > p:nth-child(3)"));
-		Assertions.assertEquals("PRICE: £5.99", createdPrice.getText());
+		WebElement createdPrice = this.driver
+				.findElement(By.cssSelector("#root > div > div > div > div > div > div > h6"));
+		Assertions.assertEquals("£ 5.99", createdPrice.getText());
 
 	}
 
-	@Test
-	@Order(3)
-	void deleteItems() {
-		this.driver.get("http://localhost:3000/item");
-
-		WebElement Delete = this.driver.findElement(
-				By.cssSelector("body > div > div:nth-child(3) > div > div > div:nth-child(1) > button:nth-child(4)"));
-		this.driver.executeScript("arguments[0].scrollIntoView(true);", Delete);
-		this.driver.executeScript("arguments[0].click();", Delete);
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-//		
-
-		wait.until(ExpectedConditions.invisibilityOf(Delete));
-		List<WebElement> items = this.driver
-				.findElements(By.cssSelector("body > div > div:nth-child(3) > div > div > div:nth-child(1) > div"));
-		Assertions.assertEquals(1, items.size());
-	}
+//	@Test
+//	@Order(3)
+//	void deleteItems() {
+//		this.driver.get("http://localhost:3000/item");
+//
+//		WebElement Delete = this.driver.findElement(
+//				By.cssSelector("body > div > div:nth-child(3) > div > div > div:nth-child(1) > button:nth-child(4)"));
+//		this.driver.executeScript("arguments[0].scrollIntoView(true);", Delete);
+//		this.driver.executeScript("arguments[0].click();", Delete);
+//
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+////		
+//
+//		wait.until(ExpectedConditions.invisibilityOf(Delete));
+//		List<WebElement> items = this.driver
+//				.findElements(By.cssSelector("body > div > div:nth-child(3) > div > div > div:nth-child(1) > div"));
+//		Assertions.assertEquals(1, items.size());
+//	}
 
 }
